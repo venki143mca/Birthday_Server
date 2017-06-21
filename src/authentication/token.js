e74fcfa6804d4b80a154ebaf8fa9a83696703786
c_sverao <c_sverao@qualcomm.com>
2017-06-21 11:50:04 -0700

    changes

--- src/authentication/token.js
+++ src/authentication/token.js
@@ -5,7 +5,6 @@ function isAuthenticated(req, res, next) {
     console.log("Middleware::Authentication");
     const token = req.headers['x-access-token'];
     const decoded = jwt.decode(token, "YOUR_SECRET_STRING");
-    console.log(decoded);
     const d = Date.now();
     if (decoded.exp <= d) {
         res.send('Access token has expired.', 403);
@@ -14,7 +13,7 @@ function isAuthenticated(req, res, next) {
 }
 
 function generateToken(user) {
-    const expires = moment().add('minutes', 5).valueOf();
+    const expires = moment().add(1, 'hours').valueOf();
     console.log(user);
 
     var token = jwt.encode({
@@ -26,6 +25,16 @@ function generateToken(user) {
         token: token,
         expires: expires
     }
+}
+
+function invalidateToken() {
+//     Create a token blacklist
+
+// You could store the invalid tokens until their initial expiry date, and compare them against incoming requests. 
+// This seems to negate the reason for going fully token based in the first place though, 
+// as you would need to touch the database for every request. The storage size would likely be lower though, 
+// as you would only need to store tokens that were between logout & expiry time (this is a gut feeling, 
+// and is definitely dependent on context).
 }
 module.exports = {
     isAuthenticated,
